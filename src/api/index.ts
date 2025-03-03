3888c7cdf2abaf9299913b7cddad1d41f1616021
Kam <Kam@KamranT.com>
2025-03-02 22:54:31 -0800

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/api/", (c) => c.json({ name: "0.0.0.0" }));
 
 app.get("*", (c) => {
   return c.env.ASSETS.fetch(c.req.raw);
